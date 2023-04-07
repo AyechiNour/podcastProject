b8011c5aca5eb3600d33908d940bf2e17f4f6f98
AyechiNour <ayechii.nourr@gmail.com>
2023-04-07 21:43:30 +0100

    Initial commit

---
+++ src/pages/dashboard/tables.jsx
@@ -0,0 +1,252 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Avatar,
+  Chip,
+  Tooltip,
+  Progress,
+  Input,
+  Button,
+} from "@material-tailwind/react";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
+import { authorsTableData, projectsTableData } from "@/data";
+import { Link } from "react-router-dom";
+
+export function Tables() {
+  return (
+    <div className="mt-12 mb-8 flex flex-col gap-12">
+      <Card>
+        <CardHeader variant="gradient" color="blue" className="mb-8 p-6">
+          <Typography variant="h6" color="white">
+            Add New Article
+          </Typography>
+        </CardHeader>
+        <CardBody className="overflow-x-scroll px-6 pt-0 pb-2">
+          <Typography variant="h5" color="blue-gray" className="mb-1">
+            Enter the subject
+          </Typography>
+          <Typography className="text-xs font-normal text-blue-gray-500">
+            let me know what subject you would like me to assist you with.
+          </Typography>
+          <div className="mr-auto my-6 flex flex-row justify-center items-center">
+            <Input label="Type here"/>
+            <Link className="ml-2" >
+              <Button variant="outlined" size="sm">
+                Search
+              </Button>
+            </Link>
+          </div>
+          <div className="w-full border h-52 rounded-md overflow-y-scroll"></div>
+          <Link className="mt-2 flex justify-end">
+              <Button variant="outlined" size="sm">
+                Validate
+              </Button>
+            </Link>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardHeader variant="gradient" color="blue" className="mb-8 p-6">
+          <Typography variant="h6" color="white">
+            Articles Table
+          </Typography>
+        </CardHeader>
+        <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+          <table className="w-full min-w-[640px] table-auto">
+            <thead>
+              <tr>
+                {["author", "function", "status", "employed", ""].map((el) => (
+                  <th
+                    key={el}
+                    className="border-b border-blue-gray-50 py-3 px-5 text-left"
+                  >
+                    <Typography
+                      variant="small"
+                      className="text-[11px] font-bold uppercase text-blue-gray-400"
+                    >
+                      {el}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {authorsTableData.map(
+                ({ img, name, email, job, online, date }, key) => {
+                  const className = `py-3 px-5 ${key === authorsTableData.length - 1
+                    ? ""
+                    : "border-b border-blue-gray-50"
+                    }`;
+
+                  return (
+                    <tr key={name}>
+                      <td className={className}>
+                        <div className="flex items-center gap-4">
+                          <Avatar src={img} alt={name} size="sm" />
+                          <div>
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="font-semibold"
+                            >
+                              {name}
+                            </Typography>
+                            <Typography className="text-xs font-normal text-blue-gray-500">
+                              {email}
+                            </Typography>
+                          </div>
+                        </div>
+                      </td>
+                      <td className={className}>
+                        <Typography className="text-xs font-semibold text-blue-gray-600">
+                          {job[0]}
+                        </Typography>
+                        <Typography className="text-xs font-normal text-blue-gray-500">
+                          {job[1]}
+                        </Typography>
+                      </td>
+                      <td className={className}>
+                        <Chip
+                          variant="gradient"
+                          color={online ? "green" : "blue-gray"}
+                          value={online ? "online" : "offline"}
+                          className="py-0.5 px-2 text-[11px] font-medium"
+                        />
+                      </td>
+                      <td className={className}>
+                        <Typography className="text-xs font-semibold text-blue-gray-600">
+                          {date}
+                        </Typography>
+                      </td>
+                      <td className={className}>
+                        <Typography
+                          as="a"
+                          href="#"
+                          className="text-xs font-semibold text-blue-gray-600"
+                        >
+                          Edit
+                        </Typography>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </CardBody>
+      </Card>
+      {/* <Card>
+        <CardHeader variant="gradient" color="blue" className="mb-8 p-6">
+          <Typography variant="h6" color="white">
+            Projects Table
+          </Typography>
+        </CardHeader>
+        <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+          <table className="w-full min-w-[640px] table-auto">
+            <thead>
+              <tr>
+                {["companies", "members", "budget", "completion", ""].map(
+                  (el) => (
+                    <th
+                      key={el}
+                      className="border-b border-blue-gray-50 py-3 px-5 text-left"
+                    >
+                      <Typography
+                        variant="small"
+                        className="text-[11px] font-bold uppercase text-blue-gray-400"
+                      >
+                        {el}
+                      </Typography>
+                    </th>
+                  )
+                )}
+              </tr>
+            </thead>
+            <tbody>
+              {projectsTableData.map(
+                ({ img, name, members, budget, completion }, key) => {
+                  const className = `py-3 px-5 ${
+                    key === projectsTableData.length - 1
+                      ? ""
+                      : "border-b border-blue-gray-50"
+                  }`;
+
+                  return (
+                    <tr key={name}>
+                      <td className={className}>
+                        <div className="flex items-center gap-4">
+                          <Avatar src={img} alt={name} size="sm" />
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-bold"
+                          >
+                            {name}
+                          </Typography>
+                        </div>
+                      </td>
+                      <td className={className}>
+                        {members.map(({ img, name }, key) => (
+                          <Tooltip key={name} content={name}>
+                            <Avatar
+                              src={img}
+                              alt={name}
+                              size="xs"
+                              variant="circular"
+                              className={`cursor-pointer border-2 border-white ${
+                                key === 0 ? "" : "-ml-2.5"
+                              }`}
+                            />
+                          </Tooltip>
+                        ))}
+                      </td>
+                      <td className={className}>
+                        <Typography
+                          variant="small"
+                          className="text-xs font-medium text-blue-gray-600"
+                        >
+                          {budget}
+                        </Typography>
+                      </td>
+                      <td className={className}>
+                        <div className="w-10/12">
+                          <Typography
+                            variant="small"
+                            className="mb-1 block text-xs font-medium text-blue-gray-600"
+                          >
+                            {completion}%
+                          </Typography>
+                          <Progress
+                            value={completion}
+                            variant="gradient"
+                            color={completion === 100 ? "green" : "blue"}
+                            className="h-1"
+                          />
+                        </div>
+                      </td>
+                      <td className={className}>
+                        <Typography
+                          as="a"
+                          href="#"
+                          className="text-xs font-semibold text-blue-gray-600"
+                        >
+                          <EllipsisVerticalIcon
+                            strokeWidth={2}
+                            className="h-5 w-5 text-inherit"
+                          />
+                        </Typography>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </CardBody>
+      </Card> */}
+    </div>
+  );
+}
+
+export default Tables;
